041a3066a42ab941c7a48b6565024ba3b1a83d48
lcw3176 <lcw3176@naver.com>
2023-06-18 18:50:29 +0900

    feat: router 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-layout>
+    <v-app-bar elevation="1" v-if="display.mdAndUp">
+      <v-btn class="font-weight-black font-weight-bold text-h5" to="/">
+        {{ appTitle }}
+
+      </v-btn>
+
+      <v-btn v-for="item in desktop" :key="item.title" size="large" variant="plain" :to="item.path">
+
+        {{ item.title }}
+
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-bottom-navigation v-else grow>
+      <v-btn v-for="item in mobile" :key="item.title" color="teal" :to="item.path">
+        <v-icon>{{ item.icon }}</v-icon>
+        {{ item.title }}
+      </v-btn>
+    </v-bottom-navigation>
+
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+
+  </v-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { useDisplay } from 'vuetify'
+import { ref } from 'vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    const display = ref(useDisplay())
+
+    return {
+      appTitle: "MAPSHOT ADMIN",
+      sidebar: false,
+      desktop: [
+        { title: "사용법", path: "/manual" },
+        { title: "공지사항", path: "/notice" },
+        { title: "문의", path: "/contact" },
+      ],
+
+
+      mobile: [
+        { title: "홈", path: "/", icon: "mdi-home-outline" },
+        { title: "사용법", path: "/manual", icon: "mdi-school-outline" },
+        { title: "공지사항", path: "/notice", icon: "mdi-bullhorn-outline" },
+        { title: "문의", path: "/contact", icon: "mdi-tooltip-question-outline" },
+        
+      ],
+      display
+    };
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createPinia } from "pinia"
 
-createApp(App).mount('#app')
+
+// Vuetify
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(createPinia()).use(router).use(vuetify).mount('#app')
