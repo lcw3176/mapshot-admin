4874bf6dc3107cc01a1df09ef71b279e1db934af
lcw3176 <lcw3176@naver.com>
2024-04-30 17:14:28 +0900

    인피니티 로딩 제거

--- src/views/NoticeModifyView.vue
+++ src/views/NoticeModifyView.vue
@@ -16,9 +16,6 @@
         </v-sheet>
     </v-container>
 
-    <InfiniteLoading @infinite="noticeStore.infiniteHandler">
-
-    </InfiniteLoading>
 </template>
 
 
@@ -45,7 +42,7 @@ export default {
     },
 
     created() {
-        this.noticeStore.getNoticeDetail(this.noticeNumber);
+        this.noticeStore.loadPost(this.noticeNumber);
     },
 
 
